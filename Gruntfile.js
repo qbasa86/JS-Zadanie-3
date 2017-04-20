cae4dc767e555c81529f7da3bc13c2815036c53f
qbasa <qbasa@wp.pl>
2017-04-20 15:16:28 +0200

    wysyłka gotowego zadania

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  
+  jshint: {
+      all: ['js/*.js']
+    }
+  });
+  // Load the plugins tasks 
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  
+  // Default task(s).
+  grunt.registerTask('default', ['jshint']);
+};
---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var name = prompt("Wpisz swoje imię");//zmianna name
+alert("Siema, " + name);//funkcja alert
+console.log("witaj, " + name);/*Blokowy
+komentarz*/
